3b9b1dc7d3b5b58872c7cba45c0611ef8545be8a
Shantanu Kumar kar <77162449+shantanu-crypto@users.noreply.github.com>
2022-10-15 00:12:20 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.30%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     int xorOperation(int n, int start) {
         int nums[n];
-        for(int i=0;i<n;i++){
-            nums[i]=start+2*i;
-        }
-        
         int XOR=0;
         for(int i=0;i<n;i++){
-            XOR=XOR^nums[i];
+            XOR=XOR^(start+2*i);
         }
         return XOR;
     }
