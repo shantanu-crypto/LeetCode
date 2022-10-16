0fc0274789c9a436e533b7e079cbb82929881bc9
Shantanu Kumar kar <77162449+shantanu-crypto@users.noreply.github.com>
2022-10-16 18:05:23 +0530

    Time: 18 ms (54.06%), Space: 10.2 MB (46.26%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    static bool compare(int a,int b){
+        int x=__builtin_popcount(a);
+        int y=__builtin_popcount(b);
+        
+        if(x!=y){
+            return x<y;
+        }
+        return a<b;
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+        
+        sort(arr.begin(),arr.end(),compare);
+        return arr;
+    }
+};
