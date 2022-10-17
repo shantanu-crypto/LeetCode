98fcfb8ff85cfaa5fdd467ee7dd63047a21fd2a3
Shantanu Kumar kar <77162449+shantanu-crypto@users.noreply.github.com>
2022-10-17 14:46:33 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     
-    int f(int ind,vector<int> &dp){
-        if(ind==0) return 1;
-        if(ind==1) return 1;
+//     int f(int ind,vector<int> &dp){
+//         if(ind==0) return 1;
+//         if(ind==1) return 1;
         
-        if(dp[ind]!=-1) return dp[ind];
-        int one=f(ind-1,dp);
-        int two=f(ind-2,dp);
+//         if(dp[ind]!=-1) return dp[ind];
+//         int one=f(ind-1,dp);
+//         int two=f(ind-2,dp);
         
-        return dp[ind]=one+two;
-    }
+//         return dp[ind]=one+two;
+//     }
     
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
         // f(n,dp);
-        return f(n,dp);
+        dp[0]=1;dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
