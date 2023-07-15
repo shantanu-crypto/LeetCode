77f9170caa61549d6b21f2b036e252029677b7e3
Shantanu Kumar kar <77162449+shantanu-crypto@users.noreply.github.com>
2023-07-15 12:41:05 +0530

    Time: 49 ms (29.31%), Space: 19 MB (73.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        sort(intervals.begin(),intervals.end());
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
         vector<vector<int>> ans;
-        
         for(int i=0;i<n;i++){
-            if(ans.empty() || ans.back()[1]<intervals[i][0]){
-                ans.push_back(intervals[i]);
-            }else{
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            int start=arr[i][0];
+            int end=arr[i][1];
+            if(!ans.empty() && ans.back()[1]>=end){
+                continue;
             }
+            for(int j=i+1;j<n;j++){
+                if(arr[j][0]<=end){
+                    end=max(end,arr[j][1]);
+                }else{
+                    break;
+                }
+            }
+            ans.push_back({start,end});
         }
         return ans;
     }
